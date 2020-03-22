1acffd0302a1959c278a415a58ce8447d4df898c
Mikhail Shilo <mikhailshilo@gmail.com>
2020-03-22 14:09:50 +0700

    добавили три пустых компонента

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import CreateEmployeeForm from './components/CreateEmployeeForm';
+import ViewEmployees from './components/ViewEmployees';
+import Settings from './components/Settings';
 
 function App() {
+  //////
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <CreateEmployeeForm /> 
+      <ViewEmployees/> 
+      <Settings/>  
     </div>
   );
 }
---
+++ src/components/ViewEmployees.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function ViewEmployees () {
+    return (
+        <div>ViewEmployees</div>
+    )
+};
+
+export default ViewEmployees;
